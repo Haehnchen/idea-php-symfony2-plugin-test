facbba85a85f1032d2ea4a39d632ed081d47beb9
Daniel Espendiller <daniel@espendiller.net>
2014-11-30 11:31:43 +0100

    add embed and include function testing

--- src/espend/Twig/TypeBundle/Controller/DefaultController.php
+++ src/espend/Twig/TypeBundle/Controller/DefaultController.php
@@ -22,8 +22,63 @@ class DefaultController extends Controller
 
     public function quickFixAction()
     {
+        $car = $this->getDoctrine()->getRepository('espendDoctrineModelBundle:Car')->find(1);
+        $cars = $this->getDoctrine()->getRepository('espendDoctrineModelBundle:Car')->findAll();
+
+
+
+        $car.not
+
+
+        if (isset($car->getId())) {
+
+        }
+
+        print $cars;
+
+        if (isset($cars)) {
+
+        }
+
+        if (isset($car['asas'])) {
+
+        }
+
+        $x = count($cars['as']);
+        for ($i = 1; $i <= $x; $i++) {
+
+        }
+
+        if (isset($this->indexAction())) {
+
+        }
+
+        try {
+            $cars;
+        } catch (\Exception $e) {
+
+        }
+        if (!$cars) {
+
+        }
+
+        if ($cars === null) {
+
+        }
+
+        try {
+            $this->getDoctrine()->getRepository('espendDoctrineModelBundle:Car')->findAll();
+        } catch (\Exception $e) {
+
+        }
+
+
         // should build a file which already have extends and block collected on same directory twig files
-        $this->render('espendTwigTypeBundle:Quickfix:foo2.html.twig');
+        return $this->render('espendTwigTypeBundle:Quickfix:foo2.html.twig', array(
+            'name' => 'foo',
+            'car' => $car,
+            'cars' => $cars,
+        ));
     }
 
 }
